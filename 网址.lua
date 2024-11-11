12a258c3b2ee5d7433ac52fe0c85c6a33709186f
XGOHUBlIllIlIIIIlllllIIlIlIIIIIllllIIlL <3506528177@qq.com>
2024-11-12 01:00:59 +0800

    Update 网址.lua

--- 网址.lua
+++ 网址.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/3uAvazYu/raw"))()Wait(0.1)
+--loadstring(game:HttpGet("https://pastefy.app/3uAvazYu/raw"))()
 loadstring(game:HttpGet("https://pastefy.app/hGQSVnPR/raw"))()
