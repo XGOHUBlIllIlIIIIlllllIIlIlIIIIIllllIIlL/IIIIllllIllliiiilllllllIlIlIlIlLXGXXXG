975a258aa22648d7dcf9733e8e9e1de65308d845
XGOHUBlIllIlIIIIlllllIIlIlIIIIIllllIIlL <3506528177@qq.com>
2024-11-13 04:55:29 +0800

    Update 网址.lua

--- 网址.lua
+++ 网址.lua
@@ -1,3 +1,4 @@
 --loadstring(game:HttpGet("https://pastefy.app/3uAvazYu/raw"))()
 --loadstring(game:HttpGet("https://pastefy.app/hGQSVnPR/raw"))()
-loadstring(game:HttpGet("https://pastefy.app/6vAFcZAG/raw"))()
+--loadstring(game:HttpGet("https://pastefy.app/6vAFcZAG/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/lWQYtQKl/raw"))()
