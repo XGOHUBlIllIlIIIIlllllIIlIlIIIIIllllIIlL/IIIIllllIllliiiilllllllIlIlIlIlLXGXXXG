a956e4d76550c84742f22496c2b0b06657e1bc79
lilililililililililililil <3506528177@qq.com>
2024-12-27 20:36:39 +0800

    Add files via upload
    Signed-off-by: lilililililililililililil <3506528177@qq.com>

--- UI.XGOHUB.lua
+++ UI.XGOHUB.lua
@@ -4610,9 +4610,9 @@ function Library:CreateWindow(setup)
     InputField.BackgroundTransparency = 0.5
     InputField.BorderColor3 = Color3.fromRGB(0, 0, 0)
     InputField.BorderSizePixel = 0
-    InputField.Position = UDim2.new(0.05, 0, 0.4, 0) -- 输入框放置在滑块左侧
-    InputField.Size = UDim2.new(0.05, 0, 0.4, 0)
-    InputField.ZIndex = 15
+    InputField.Position = UDim2.new(0, 0, 0.5, 0) -- 输入框放置在滑块的左侧，垂直居中
+    InputField.Size = UDim2.new(0.05, 0, 0.4, 0) -- 输入框高度增加一倍
+    InputField.TextSize = 18 -- 增加字体大小
     InputField.Font = Enum.Font.Gotham
     InputField.TextColor3 = Color3.fromRGB(255, 255, 255) -- 白色字体
     InputField.TextScaled = true
