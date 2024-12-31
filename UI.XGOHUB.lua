ff31d32fd4b2f085bd7b80bb5f27c38601d84b3d
lilililililililililililil <3506528177@qq.com>
2025-01-01 01:48:11 +0800

    Add files via upload
    Signed-off-by: lilililililililililililil <3506528177@qq.com>

--- UI.XGOHUB.lua
+++ UI.XGOHUB.lua
@@ -3727,7 +3727,7 @@ function Library:Windowxgo(setup)
                 TextLabel.TextColor3 = newTextColor;
                 TextLabel.TextStrokeColor3 = newTextColor; -- 更新文本和描边颜色
             end;
-            
+           --[[ 
             if colorEffect then
                 local function zigzag(X)
                     return math.acos(math.cos(X * math.pi)) / math.pi
@@ -3745,10 +3745,150 @@ function Library:Windowxgo(setup)
                 end
 
                 colorChange(TextLabel)
-            end
+            end]]
+if colorEffect then
+        local function colorChange(textLabel)
+            local hue = 0
+            spawn(function()
+                while true do
+                    wait(0.03)
+                    -- 将 hue 值从 0 增加到 360，然后循环
+                    hue = (hue + 1) % 360
+                    textLabel.TextColor3 = Color3.fromHSV(hue / 360, 1, 1)
+                end
+            end)
+        end
 
+        colorChange(TextLabel)
+    end
+    
             return RootSkid;
         end;
+------ // 信息标签   ----------------------------------------------------------------------------------------
+		function Root:Paragraph(Setup)
+			Setup = Setup or {};
+			Setup.Title = Setup.Title
+			Setup.Description = Setup.Description or "";
+
+			local ParagraphBlock = Instance.new("Frame")
+			local DropShadow = Instance.new("ImageLabel")
+			local UIStroke = Instance.new("UIStroke")
+			local Title = Instance.new("TextLabel")
+			local Description = Instance.new("TextLabel")
+
+			ParagraphBlock.Name = "ParagraphBlock"
+			ParagraphBlock.Parent = ScrollingFrame
+			ParagraphBlock.BackgroundColor3 = Library.Colors.Default
+			ParagraphBlock.BackgroundTransparency = 0.250
+			ParagraphBlock.BorderColor3 = Color3.fromRGB(0, 0, 0)
+			ParagraphBlock.BorderSizePixel = 0
+			ParagraphBlock.Size = UDim2.new(0.99000001, 0, 0, 24)
+			ParagraphBlock.ZIndex = 10
+
+			DropShadow.Name = "DropShadow"
+			DropShadow.Parent = ParagraphBlock
+			DropShadow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+			DropShadow.BackgroundTransparency = 1.000
+			DropShadow.BorderColor3 = Color3.fromRGB(27, 42, 53)
+			DropShadow.Position = UDim2.new(0, -5, 0, -5)
+			DropShadow.Size = UDim2.new(1, 10, 1, 10)
+			DropShadow.ZIndex = 9
+			DropShadow.Image = "rbxassetid://297694300"
+			DropShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+			DropShadow.ImageTransparency = 0.500
+			DropShadow.ScaleType = Enum.ScaleType.Slice
+			DropShadow.SliceCenter = Rect.new(95, 103, 894, 902)
+			DropShadow.SliceScale = 0.050
+
+			UIStroke.Transparency = 0.850
+			UIStroke.Color = Color3.fromRGB(156, 156, 156)
+			UIStroke.Parent = ParagraphBlock
+
+			Title.RichText = true
+			Title.Name = "Title"
+			Title.Parent = ParagraphBlock
+			Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+			Title.BackgroundTransparency = 1.000
+			Title.BorderColor3 = Color3.fromRGB(0, 0, 0)
+			Title.BorderSizePixel = 0
+			Title.Position = UDim2.new(0, 5, 0, 5)
+			Title.Size = UDim2.new(1, 0, 0, 14)
+			Title.ZIndex = 11
+			Title.Font = Enum.Font.Gotham
+			Title.Text = Setup.Title
+			Title.TextColor3 = Library.Colors.TextColor
+			Title.TextScaled = true
+			Title.TextSize = 14.000
+			Title.TextStrokeColor3 = Library.Colors.TextColor
+			Title.TextStrokeTransparency = 0.950
+			Title.TextWrapped = true
+			Title.TextXAlignment = Enum.TextXAlignment.Left
+			Title.RichText = true
+
+			Description.Name = "Description"
+			Description.Parent = ParagraphBlock
+			Description.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+			Description.BackgroundTransparency = 1.000
+			Description.BorderColor3 = Color3.fromRGB(0, 0, 0)
+			Description.BorderSizePixel = 0
+			Description.Position = UDim2.new(0, 5, 0, 21)
+			Description.Size = UDim2.new(1, 0, 0, 45)
+			Description.Visible = false
+			Description.ZIndex = 11
+			Description.Font = Enum.Font.Gotham
+			Description.Text = Setup.Description
+			Description.TextColor3 = Library.Colors.TextColor
+			Description.TextSize = 13.000
+			Description.TextStrokeColor3 = Library.Colors.TextColor
+			Description.TextStrokeTransparency = 0.950
+			Description.TextTransparency = 0.500
+			Description.TextWrapped = true
+			Description.TextXAlignment = Enum.TextXAlignment.Left
+			Description.TextYAlignment = Enum.TextYAlignment.Top
+			Description.RichText = true
+
+			local UpdateBlock = function()
+				local TitleSize = 14;
+				local MainSize = Library:GetTextSize(Description.Text,Description.TextSize,Description.Font);
+				local DescriptionSize = MainSize.Y;
+
+				Description.Size = UDim2.new(1, MainSize.X, 0, DescriptionSize + 5)
+
+				if Description.Text:byte() then
+					Description.Visible = true;
+					Library:Tween(ParagraphBlock,Library.TweenLibrary.SmallEffect,{
+						Size = UDim2.new(0.99, 0, 0, TitleSize + ((Description.Visible and Description.AbsoluteSize.Y + 5) or 0));
+					});
+
+				else
+					Description.Visible = false;
+
+					Library:Tween(ParagraphBlock,Library.TweenLibrary.SmallEffect,{
+						Size = UDim2.new(0.99, 0, 0, Title.AbsoluteSize.Y + 10);
+					});
+				end;
+			end;
+
+			UpdateBlock()
+
+			local RootSkid = {};
+
+			function RootSkid:Title(Setup)
+				Title.Text = Setup
+				UpdateBlock()
+			end;
+
+			function RootSkid:Description(Setup)
+				Description.Text = Setup
+				UpdateBlock()
+			end;
+
+			function RootSkid:Visible(value)
+				ParagraphBlock.Visible = value;
+			end;
+
+			return RootSkid;
+		end;
 --[[---- // 颜色选择器   ----------------------------------------------------------------------------------------
 local function SaveConfiguration()
     if not CEnabled then return end
@@ -5114,131 +5254,6 @@ return ColorPickerSettings
 
 			function RootSkid:Visible(value)
 				TextBoxBlock.Visible = value;
-			end;
-
-			return RootSkid;
-		end;
------- // 信息标签   ----------------------------------------------------------------------------------------
-		function Root:Paragraph(Setup)
-			Setup = Setup or {};
-			Setup.Title = Setup.Title
-			Setup.Description = Setup.Description or "";
-
-			local ParagraphBlock = Instance.new("Frame")
-			local DropShadow = Instance.new("ImageLabel")
-			local UIStroke = Instance.new("UIStroke")
-			local Title = Instance.new("TextLabel")
-			local Description = Instance.new("TextLabel")
-
-			ParagraphBlock.Name = "ParagraphBlock"
-			ParagraphBlock.Parent = ScrollingFrame
-			ParagraphBlock.BackgroundColor3 = Library.Colors.Default
-			ParagraphBlock.BackgroundTransparency = 0.250
-			ParagraphBlock.BorderColor3 = Color3.fromRGB(0, 0, 0)
-			ParagraphBlock.BorderSizePixel = 0
-			ParagraphBlock.Size = UDim2.new(0.99000001, 0, 0, 24)
-			ParagraphBlock.ZIndex = 10
-
-			DropShadow.Name = "DropShadow"
-			DropShadow.Parent = ParagraphBlock
-			DropShadow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-			DropShadow.BackgroundTransparency = 1.000
-			DropShadow.BorderColor3 = Color3.fromRGB(27, 42, 53)
-			DropShadow.Position = UDim2.new(0, -5, 0, -5)
-			DropShadow.Size = UDim2.new(1, 10, 1, 10)
-			DropShadow.ZIndex = 9
-			DropShadow.Image = "rbxassetid://297694300"
-			DropShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
-			DropShadow.ImageTransparency = 0.500
-			DropShadow.ScaleType = Enum.ScaleType.Slice
-			DropShadow.SliceCenter = Rect.new(95, 103, 894, 902)
-			DropShadow.SliceScale = 0.050
-
-			UIStroke.Transparency = 0.850
-			UIStroke.Color = Color3.fromRGB(156, 156, 156)
-			UIStroke.Parent = ParagraphBlock
-
-			Title.RichText = true
-			Title.Name = "Title"
-			Title.Parent = ParagraphBlock
-			Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-			Title.BackgroundTransparency = 1.000
-			Title.BorderColor3 = Color3.fromRGB(0, 0, 0)
-			Title.BorderSizePixel = 0
-			Title.Position = UDim2.new(0, 5, 0, 5)
-			Title.Size = UDim2.new(1, 0, 0, 14)
-			Title.ZIndex = 11
-			Title.Font = Enum.Font.Gotham
-			Title.Text = Setup.Title
-			Title.TextColor3 = Library.Colors.TextColor
-			Title.TextScaled = true
-			Title.TextSize = 14.000
-			Title.TextStrokeColor3 = Library.Colors.TextColor
-			Title.TextStrokeTransparency = 0.950
-			Title.TextWrapped = true
-			Title.TextXAlignment = Enum.TextXAlignment.Left
-			Title.RichText = true
-
-			Description.Name = "Description"
-			Description.Parent = ParagraphBlock
-			Description.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-			Description.BackgroundTransparency = 1.000
-			Description.BorderColor3 = Color3.fromRGB(0, 0, 0)
-			Description.BorderSizePixel = 0
-			Description.Position = UDim2.new(0, 5, 0, 21)
-			Description.Size = UDim2.new(1, 0, 0, 45)
-			Description.Visible = false
-			Description.ZIndex = 11
-			Description.Font = Enum.Font.Gotham
-			Description.Text = Setup.Description
-			Description.TextColor3 = Library.Colors.TextColor
-			Description.TextSize = 13.000
-			Description.TextStrokeColor3 = Library.Colors.TextColor
-			Description.TextStrokeTransparency = 0.950
-			Description.TextTransparency = 0.500
-			Description.TextWrapped = true
-			Description.TextXAlignment = Enum.TextXAlignment.Left
-			Description.TextYAlignment = Enum.TextYAlignment.Top
-			Description.RichText = true
-
-			local UpdateBlock = function()
-				local TitleSize = 14;
-				local MainSize = Library:GetTextSize(Description.Text,Description.TextSize,Description.Font);
-				local DescriptionSize = MainSize.Y;
-
-				Description.Size = UDim2.new(1, MainSize.X, 0, DescriptionSize + 5)
-
-				if Description.Text:byte() then
-					Description.Visible = true;
-					Library:Tween(ParagraphBlock,Library.TweenLibrary.SmallEffect,{
-						Size = UDim2.new(0.99, 0, 0, TitleSize + ((Description.Visible and Description.AbsoluteSize.Y + 5) or 0));
-					});
-
-				else
-					Description.Visible = false;
-
-					Library:Tween(ParagraphBlock,Library.TweenLibrary.SmallEffect,{
-						Size = UDim2.new(0.99, 0, 0, Title.AbsoluteSize.Y + 10);
-					});
-				end;
-			end;
-
-			UpdateBlock()
-
-			local RootSkid = {};
-
-			function RootSkid:Title(Setup)
-				Title.Text = Setup
-				UpdateBlock()
-			end;
-
-			function RootSkid:Description(Setup)
-				Description.Text = Setup
-				UpdateBlock()
-			end;
-
-			function RootSkid:Visible(value)
-				ParagraphBlock.Visible = value;
 			end;
 
 			return RootSkid;
